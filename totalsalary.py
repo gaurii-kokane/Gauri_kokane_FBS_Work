7a44089b9d58cec418abb7483ccb85b349a7948f
gaurii-kokane <gaurikokane6@gmail.com>
2025-08-28 08:17:22 +0530

    Delete totalsalary.py

--- totalsalary.py
+++
@@ -1,7 +0,0 @@
-#calculate total salary of employeee
-basic=float(input("enter the salary:"))
-da=0.10*basic
-ta=0.12*basic
-hra=0.15*basic
-total=basic+da+ta+hra
-print("Total salary of employee:",total)
