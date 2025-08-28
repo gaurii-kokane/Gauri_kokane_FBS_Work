567ceec7a1e5170a15555acd6424783be12b272b
gaurii-kokane <gaurikokane6@gmail.com>
2025-08-28 08:16:24 +0530

    Delete meter.py

--- meter.py
+++
@@ -1,7 +0,0 @@
-#conver feet and inches in meter and cm
-feet=float(input("Enter feet:"))
-inches=float(input("Enter inches:"))
-cm=(feet+30.48)+(inches+2.54)
-meter=cm/100
-print("Meter is :",meter)
-print("centimeter is:",cm)
